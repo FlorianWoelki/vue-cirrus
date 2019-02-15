ca36497220e12661986406b23ad8300a7fc967a2
FlorianWoelki <FlorianWoelki@gmx.de>
2019-02-15 12:49:29 +0100

    Renamed names of buttons

--- src/Demo.vue
+++ src/Demo.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="btn-container">
-    <v-btn>Normal v-btn</v-btn>
-    <v-btn animated>Animated v-btn</v-btn>
-    <v-btn animated btnStyle="accent">Animated accent v-btn</v-btn>
-    <v-btn btnStyle="outline-inverted">Inverted outline v-btn</v-btn>
-    <v-btn btnStyle="transparent">Transparent v-btn</v-btn>
-    <v-btn btnStyle="light">Light Gray v-btn</v-btn>
-    <v-btn btnStyle="dark">Dark Gray v-btn</v-btn>
-    <v-btn btnStyle="black">Black v-btn</v-btn>
-    <v-btn tiny>Tiny v-btn</v-btn>
-    <v-btn small>Small v-btn</v-btn>
-    <v-btn large>Large v-btn</v-btn>
-    <v-btn extraLarge>Extralarge v-btn</v-btn>
+    <v-btn>Normal button</v-btn>
+    <v-btn animated>Animated button</v-btn>
+    <v-btn animated btnStyle="accent">Animated accent button</v-btn>
+    <v-btn btnStyle="outline-inverted">Inverted outline button</v-btn>
+    <v-btn btnStyle="transparent">Transparent button</v-btn>
+    <v-btn btnStyle="light">Light Gray button</v-btn>
+    <v-btn btnStyle="dark">Dark Gray button</v-btn>
+    <v-btn btnStyle="black">Black button</v-btn>
+    <v-btn tiny>Tiny button</v-btn>
+    <v-btn small>Small button</v-btn>
+    <v-btn large>Large button</v-btn>
+    <v-btn extraLarge>Extralarge button</v-btn>
   </div>
 </template>
 
