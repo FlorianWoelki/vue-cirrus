2842df0b44b1600bc072ff3e40826871597f3a91
FlorianWoelki <FlorianWoelki@gmx.de>
2019-02-16 16:24:46 +0100

    Added link component

--- src/Demo.vue
+++ src/Demo.vue
@@ -14,6 +14,27 @@
       <v-btn btnStyle="accent">Second</v-btn>
       <v-btn btnStyle="accent">Third</v-btn>
     </v-bgroup>
+
+    <v-row>
+      <v-col c2>
+        <v-link>Ordinary Link</v-link>
+      </v-col>
+      <v-col c2>
+        <v-link ltr>Hover Link</v-link>
+      </v-col>
+      <v-col c2>
+        <v-link squared>Squared Link</v-link>
+      </v-col>
+      <v-col c2>
+        <v-link dSquared>Delayed Squared Link</v-link>
+      </v-col>
+      <v-col c2>
+        <v-link c>Center Link</v-link>
+      </v-col>
+      <v-col c2>
+        <v-link oc>Opposite Center Link</v-link>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
---
+++ src/components/VLink.vue
@@ -0,0 +1,87 @@
+<template>
+  <span v-if="squared" class="usquare">
+    <a :href=href :class=classes>
+      <slot></slot>
+    </a>
+  </span>
+  <span v-else-if="dSquared" class="usquare delay">
+    <a :href=href :class=classes>
+      <slot></slot>
+    </a>
+  </span>
+  <a v-else :href=href :class=classes>
+    <slot></slot>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      default: '#',
+    },
+    ltr: {
+      type: Boolean,
+      default: false,
+    },
+    rtl: {
+      type: Boolean,
+      default: false,
+    },
+    c: {
+      type: Boolean,
+      default: false,
+    },
+    oltr: {
+      type: Boolean,
+      default: false,
+    },
+    ortl: {
+      type: Boolean,
+      default: false,
+    },
+    oc: {
+      type: Boolean,
+      default: false,
+    },
+    olr: {
+      type: Boolean,
+      default: false,
+    },
+    orl: {
+      type: Boolean,
+      default: false,
+    },
+    squared: {
+      type: Boolean,
+      default: false,
+    },
+    dSquared: {
+      type: Boolean,
+      default: false,
+    },
+    underlined: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    classes() {
+      return {
+        'u u-LR': this.ltr,
+        'u u-RL': this.rtl,
+        'u u-C': this.c,
+        'utb utb-LR': this.oltr,
+        'utb utb-RL': this.ortl,
+        'utb utb-C': this.oc,
+        'utb utb-OLR': this.olr,
+        'utb utb-ORL': this.orl,
+        'utb utb-OLR sq': this.squared,
+        'utb utb-OLR delay': this.dSquared,
+        underline: this.underlined,
+      };
+    },
+  },
+};
+</script>
