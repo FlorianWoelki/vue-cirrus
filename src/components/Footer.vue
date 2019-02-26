a313dc38b969210e8a98371107c21f8ccee67938
FlorianWoelki <FlorianWoelki@gmx.de>
2019-02-26 10:52:42 +0100

    Fixed footer text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
       <v-col c4 o4>
         <p class="white font-bold" style="letter-spacing: 0; font-size: 1.3rem;">
           <span class="faded">&copy; 2019</span>
-          " Vue Cirrus"
+          Vue Cirrus
         </p>
       </v-col>
     </v-row>
