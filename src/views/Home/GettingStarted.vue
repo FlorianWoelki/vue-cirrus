2733b5bbf83b5125a572d9ee921088ff4ced35ba
FlorianWoelki <FlorianWoelki@gmx.de>
2019-02-26 10:38:10 +0100

    Added copyable to code components

--- src/views/Home/GettingStarted.vue
+++ src/views/Home/GettingStarted.vue
@@ -11,7 +11,7 @@
     <v-row>
       <h6>Install with NPM</h6>
       <p>You can use npm to install Vue Cirrus</p>
-      <v-code lang="Console" keywords="vue-cirrus,npm">
+      <v-code lang="Console" keywords="vue-cirrus,npm" copyable>
         <pre>npm install --save vue-cirrus</pre>
       </v-code>
     </v-row>
@@ -21,14 +21,14 @@
         Setting up Vue Cirrus for any project is very simple and will only require couple of lines.
       </p>
       <p>In your index.html file insert the following lines.</p>
-      <v-code lang="HTML" keywords="link">
+      <v-code lang="HTML" keywords="link" copyable>
         <pre><link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,600,700" rel="stylesheet"></pre>
         <pre><link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet"></pre>
 
         <pre><link href="https://unpkg.com/cirrus-ui" type="text/css" rel="stylesheet" /></pre>
       </v-code>
       <p>Import all Vue Cirrus components by importing it in your main.js file.</p>
-      <v-code lang="JavaScript" keywords="import">
+      <v-code lang="JavaScript" keywords="import" copyable>
         <pre>import 'vue-cirrus/dist/vue-cirrus.css';</pre>
         <pre>import 'vue-cirrus';</pre>
       </v-code>
