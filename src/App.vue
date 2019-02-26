dba6e02c6615136a6f66791b999ea440256340ed
FlorianWoelki <FlorianWoelki@gmx.de>
2019-02-26 10:50:37 +0100

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,19 @@
     <section class="padtop">
       <router-view />
     </section>
+
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
 
 export default {
   components: {
     Navbar,
+    Footer,
   },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer>
+    <v-row class="level">
+      <v-col c4 o4>
+        <p class="white font-bold" style="letter-spacing: 0; font-size: 1.3rem;">
+          <span class="faded">&copy; 2019</span>
+          " Vue Cirrus"
+        </p>
+      </v-col>
+    </v-row>
+  </footer>
+</template>
