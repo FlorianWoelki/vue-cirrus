6653d477ec85aded7353c1151cd807a3fa11bd7e
FlorianWoelki <FlorianWoelki@gmx.de>
2019-02-28 09:14:58 +0100

    Updated components list

--- src/views/Components/Components.vue
+++ src/views/Components/Components.vue
@@ -1,9 +1,51 @@
 <template>
   <v-sidebar>
-    <v-sidebar-items title="Test">
-      <v-sidebar-item>Web</v-sidebar-item>
-      <v-sidebar-item>HTML</v-sidebar-item>
-      <v-sidebar-item>CSS</v-sidebar-item>
+    <v-sidebar-items title="Components" hoverEffect>
+      <v-sidebar-item>Buttons</v-sidebar-item>
+      <v-sidebar-item>Cards</v-sidebar-item>
+      <v-sidebar-item>Dropdown</v-sidebar-item>
+      <v-sidebar-item>Frames</v-sidebar-item>
+      <v-sidebar-item>Grid</v-sidebar-item>
+      <v-sidebar-item>Modals</v-sidebar-item>
+      <v-sidebar-item>Navbar</v-sidebar-item>
+      <v-sidebar-item>Pagination</v-sidebar-item>
+      <v-sidebar-item>Placeholder</v-sidebar-item>
+      <v-sidebar-item>Sidebar</v-sidebar-item>
+      <v-sidebar-item>Tabs</v-sidebar-item>
+      <v-sidebar-item>Tiles</v-sidebar-item>
+      <v-sidebar-item>Avatars</v-sidebar-item>
+      <v-sidebar-item>Code</v-sidebar-item>
+      <v-sidebar-item>Dividers</v-sidebar-item>
+      <v-sidebar-item>Footer</v-sidebar-item>
+      <v-sidebar-item>Links</v-sidebar-item>
+      <v-sidebar-item>NextPrev</v-sidebar-item>
+      <v-sidebar-item>Parallax</v-sidebar-item>
+      <v-sidebar-item>Snackbars</v-sidebar-item>
+      <v-sidebar-item>Spaces</v-sidebar-item>
+      <v-sidebar-item>Toasts</v-sidebar-item>
+      <v-divider />
+      <v-sidebar-item>
+        <input type="checkbox" id="mixins" class="hide" name="mixinsTree">
+        <label for="mixins" class="tree-item-header">
+          <span class="icon">
+            <i class="fa fa-chevron-right small" aria-hidden="true"></i>
+          </span>
+          Mixins
+        </label>
+        <div class="tree-item-body">
+          <ul class="menu">
+            <li class="menu-item">
+              <a>Layout</a>
+            </li>
+            <li class="menu-item">
+              <a>Tooltips</a>
+            </li>
+            <li class="menu-item">
+              <a>Animations</a>
+            </li>
+          </ul>
+        </div>
+      </v-sidebar-item>
     </v-sidebar-items>
     <v-sidebar-content>
       <h3>Test</h3>
