779e166a5f1bf950f115eae15c7ee160d9ac668a
FlorianWoelki <FlorianWoelki@gmx.de>
2019-03-22 18:21:32 +0100

    added vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import store from './store/store';
 
 import 'vue-cirrus/dist/vue-cirrus.css';
 import 'vue-cirrus';
@@ -9,5 +10,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app');
