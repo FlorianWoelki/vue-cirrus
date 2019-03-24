c10a99ec1af9341faff99041c8a40fc1d97909ac
FlorianWoelki <FlorianWoelki@gmx.de>
2019-03-24 15:27:32 +0100

    updated code component

--- src/views/Components/Code/VCode.vue
+++ src/views/Components/Code/VCode.vue
@@ -6,7 +6,7 @@
     <v-row>
       <v-col c6>
         <v-code lang="Vue">
-          <pre v-html="code"></pre>
+          <span v-html="code"></span>
         </v-code>
       </v-col>
       <v-col
@@ -16,6 +16,25 @@
         <v-code
           lang="JavaScript"
         >const myBool = true;
+if (myBool) {
+  console.log("Bool is true!");
+}</v-code>
+      </v-col>
+    </v-row>
+
+    <v-space xlarge />
+
+    <v-row>
+      <v-col c6>
+        <v-code lang="Vue">
+          <span v-html="darkCode"></span>
+        </v-code>
+      </v-col>
+      <v-col
+        c4
+        o1
+      >
+        <v-code lang="JavaScript" dark>const myBool = true;
 if (myBool) {
   console.log("Bool is true!");
 }</v-code>
@@ -49,9 +68,14 @@ export default {
 if (myBool) {
   console.log("Bool is true!");
 }</v-code></xmp>`,
+      darkCode: `<xmp><v-code lang="JavaScript" dark>const myBool = true;
+if (myBool) {
+  console.log("Bool is true!");
+}</v-code></xmp>`,
       props: {
-        lang: ['lang', 'empty', 'String', 'Language for this code segment (Supported languages: html, javascript)'],
+        lang: ['lang', 'empty', 'String', 'Language for this code segment (Supported languages: html, javascript, css)'],
         copyable: ['copyable', 'false', 'Boolean', 'If code segment is copyable'],
+        dark: ['dark', 'false', 'Boolean', 'Dark mode for code component'],
       },
     };
   },
