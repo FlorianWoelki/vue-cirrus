f91ec9c5683d405a81d093c01f74df07f2ccf2c3
FlorianWoelki <FlorianWoelki@gmx.de>
2019-03-26 10:44:28 +0100

    updated view code of snackbars

--- src/views/Components/Snackbars/VSnackbars.vue
+++ src/views/Components/Snackbars/VSnackbars.vue
@@ -14,59 +14,37 @@
         style="margin-top: 25px;"
       >
         <v-row center style="margin-bottom: 25px;">
-          <v-col c6>
-            <input type="radio" v-model=position name="position" value="0" checked> Bottom Center
-          </v-col>
-          <v-col c6>
-            <input type="radio" v-model=position name="position" value="1"> Bottom Left
-          </v-col>
-          <v-col c6>
-            <input type="radio" v-model=position  name="position" value="2"> Bottom Right
-          </v-col>
-          <v-col c6>
-            <input type="radio" v-model=position  name="position" value="3"> Top Left
-          </v-col>
-          <v-col c6>
-            <input type="radio" v-model=position  name="position" value="4"> Top Center
-          </v-col>
-          <v-col c6>
-            <input type="radio" v-model=position  name="position" value="5"> Top Right
+          <v-col
+            c6
+            v-for="(p, index) in positions"
+            :key=p
+          >
+            <input
+              type="radio"
+              v-model=position name="position"
+              :value=index
+              :checked="index == 0"
+            > {{p}}
           </v-col>
         </v-row>
-        <v-row center>
+        <v-row>
           <v-btn :onClick="() => { snackbar = !snackbar; }">Show!</v-btn>
         </v-row>
-        <!-- Sloppy solution.. TODO: Fix -->
-        <v-snackbar
-          v-if="position == 0"
-          bottomCenter
-          v-model="snackbar"
-        >Awesome Snackbar!</v-snackbar>
-        <v-snackbar
-          v-if="position == 1"
-          bottomLeft
-          v-model="snackbar"
-        >Awesome Snackbar!</v-snackbar>
-        <v-snackbar
-          v-if="position == 2"
-          bottomRight
-          v-model="snackbar"
-        >Awesome Snackbar!</v-snackbar>
-        <v-snackbar
-          v-if="position == 3"
-          topLeft
-          v-model="snackbar"
-        >Awesome Snackbar!</v-snackbar>
-        <v-snackbar
-          v-if="position == 4"
-          topCenter
-          v-model="snackbar"
-        >Awesome Snackbar!</v-snackbar>
-        <v-snackbar
-          v-if="position == 5"
-          topRight
-          v-model="snackbar"
-        >Awesome Snackbar!</v-snackbar>
+        <div
+          v-for="i in 5"
+          :key=i
+        >
+          <v-snackbar
+            v-if="i-1 == position"
+            :bottomCenter="i-1 == 0"
+            :bottomLeft="i-1 == 1"
+            :bottomRight="i-1 == 2"
+            :topLeft="i-1 == 3"
+            :topCenter="i-1 == 4"
+            :topRight="i-1 == 5"
+            v-model="snackbar"
+          >Awesome Snackbar!</v-snackbar>
+        </div>
       </v-col>
     </v-row>
 
@@ -94,6 +72,14 @@ export default {
   data() {
     return {
       position: 0,
+      positions: [
+        'Bottom Center',
+        'Bottom Left',
+        'Bottom Right',
+        'Top Left',
+        'Top Center',
+        'Top Right',
+      ],
       snackbar: false,
       code: `<xmp><v-snackbar
   bottomCenter
