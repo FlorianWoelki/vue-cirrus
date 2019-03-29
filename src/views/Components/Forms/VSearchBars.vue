48acbd4f2fda9656c366229046abe913abe077e8
FlorianWoelki <FlorianWoelki@gmx.de>
2019-03-29 11:20:13 +0100

    updated view of search bar

--- src/views/Components/Forms/VSearchBars.vue
+++ src/views/Components/Forms/VSearchBars.vue
@@ -11,13 +11,17 @@
       <v-col
         center
         c6
-        style="margin-top: 55px;"
+        style="margin-top: 30px;"
       >
         <v-container>
           <v-search-bar
             v-model=searchingWord
             :searchAction=click
           >Search Me!</v-search-bar>
+          <p>Searching for word:
+            <span v-if="searchingWord !== ''">{{searchingWord}}</span>
+            <span v-else>...</span>
+          </p>
         </v-container>
       </v-col>
     </v-row>
