2b8e8ef06f1d43051220aa7e13b48ebde4f4d527
FlorianWoelki <FlorianWoelki@gmx.de>
2019-03-31 12:12:00 +0200

    updated footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer>
+  <footer class="footer-section">
     <v-row class="level">
       <v-col
         c4
@@ -12,7 +12,31 @@
           <span class="faded">&copy; 2019</span>
           Vue Cirrus
         </p>
+
+        <v-divider />
+
+        <p>
+          Code is licensed with
+          <span class="white">MIT</span>. Everything is <span class="white">free</span> to use!
+        </p>
+
+        <p>
+          Made with <i
+            class="fa fa-heart animated pulse"
+            aria-hidden="true"
+          ></i> by <span class="white">Florian Woelki</span>
+        </p>
       </v-col>
     </v-row>
   </footer>
 </template>
+
+<style lang="scss">
+.footer-section {
+  .fa-heart {
+    color: #E90606;
+    margin: 0 3px;
+    font-size: 16px;
+  }
+}
+</style>
