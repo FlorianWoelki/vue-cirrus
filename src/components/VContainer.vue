64e2dc7396ef0139b662de5d0a45bf8ec28b927c
FlorianWoelki <FlorianWoelki@gmx.de>
2019-08-13 09:12:25 +0200

    added mixins to container component

--- src/components/VContainer.vue
+++ src/components/VContainer.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="content">
+  <div :class="classes">
     <slot></slot>
   </div>
 </template>
+
+<script>
+import Layout from '@/mixins/layout';
+import Tooltip from '@/mixins/tooltip';
+import Animations from '@/mixins/animations';
+
+export default {
+  mixins: [
+    Layout,
+    Tooltip,
+    Animations,
+  ],
+
+  computed: {
+    classes() {
+      return Object.assign(
+        this.layoutMixins,
+        this.tooltipMixins,
+        this.animationsMixins,
+        {
+          content: true,
+        },
+      );
+    },
+  },
+};
+</script>
