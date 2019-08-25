b31698573d8f1e069be75a884d9cf5d722a2b135
FlorianWoelki <FlorianWoelki@gmx.de>
2019-08-25 11:52:16 +0200

    updated icon of sidebar on mobile view

--- src/components/Sidebar/VSidebar.vue
+++ src/components/Sidebar/VSidebar.vue
@@ -6,7 +6,11 @@
         class="hide-desktop"
       >
         <span class="icon">
-          <p>Sidebar</p>
+          <div class="sidebar-icon-container">
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+          </div>
         </span>
       </a>
     </div>
@@ -43,5 +47,22 @@ export default {
 <style>
 body.sidebar {
   overflow-y: hidden;
+}
+
+.hide-desktop .icon {
+  display: initial;
+}
+
+.sidebar-menu-container {
+  display: inline-block;
+  cursor: pointer;
+}
+
+.bar1, .bar2, .bar3 {
+  width: 35px;
+  height: 5px;
+  background-color: #333;
+  margin: 6px 0;
+  transition: 0.4s;
 }
 </style>
