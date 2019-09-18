f9ec5f16489fbf1d88c9a57a4f8d85cf288d1400
FlorianWoelki <FlorianWoelki@gmx.de>
2019-09-18 20:24:16 +0200

    added first text to contact page

--- src/views/Examples/ContactPage/ContactPage.vue
+++ src/views/Examples/ContactPage/ContactPage.vue
@@ -1,3 +1,19 @@
 <template>
-  <div class="contact-page"></div>
+  <div class="contact-page">
+    <v-container class="u-text-center">
+      <h3 class="font-light">Get In Touch</h3>
+      <p class="font-light u-center">
+        We're here for you! Just drop us a dope message or a question.
+        We're really looking forward to answer you!
+      </p>
+    </v-container>
+  </div>
 </template>
+
+<style>
+.contact-page p {
+  max-width: 400px;
+  margin-top: 40px;
+  margin-bottom: 40px;
+}
+</style>
