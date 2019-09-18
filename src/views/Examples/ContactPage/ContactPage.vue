829ecf2c0e1a06aaae9179471261508d042ab303
FlorianWoelki <FlorianWoelki@gmx.de>
2019-09-18 22:11:10 +0200

    added form for contact page

--- src/views/Examples/ContactPage/ContactPage.vue
+++ src/views/Examples/ContactPage/ContactPage.vue
@@ -6,14 +6,52 @@
         We're here for you! Just drop us a dope message or a question.
         We're really looking forward to answer you!
       </p>
+      <div class="u-center">
+        <v-input-field large placeholder="Name..."></v-input-field>
+      </div>
+      <div class="u-center">
+        <v-input-field large placeholder="Email..."></v-input-field>
+      </div>
+      <div class="u-center">
+        <v-input-field large placeholder="Regarding..."></v-input-field>
+      </div>
+      <div class="u-center">
+        <v-text-area large placeholder="Message... (10 character minimum)"></v-text-area>
+      </div>
+      <div id="send-updates-checkbox">
+        <v-checkbox class="font-light">
+          Send me updates about Vue Cirrus Products and services.
+        </v-checkbox>
+      </div>
+      <v-btn primary center large>Send it!</v-btn>
     </v-container>
   </div>
 </template>
 
-<style>
-.contact-page p {
-  max-width: 400px;
-  margin-top: 40px;
-  margin-bottom: 40px;
+<style lang="scss">
+.contact-page {
+  #send-updates-checkbox {
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+
+  .content {
+    margin-top: 125px;
+  }
+
+  textarea {
+    font-size: 1.5rem;
+  }
+
+  p {
+    max-width: 400px;
+    margin-top: 40px;
+    margin-bottom: 40px;
+  }
+
+  .input-control,
+  textarea {
+    width: 500px;
+  }
 }
 </style>
