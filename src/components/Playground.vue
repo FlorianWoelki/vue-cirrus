b03c6225cf0d1e180519f83da3fbe8bc059d4b39
FlorianWoelki <FlorianWoelki@gmx.de>
2019-09-22 14:06:36 +0200

    updated dropdown to dropdown components

--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -2,15 +2,15 @@
   <v-container>
     <h3>Playground</h3>
     <v-btn center>Customize Me</v-btn>
-    <div class="list-dropdown dropdown-right">
-      <button class="btn-dropdown u-no-margin">
-        Size <span class="icon"><i class="fa fa-wrapper fa-caret-down"></i></span>
-      </button>
-      <ul class="menu">
-        <v-dropdown-item>Test1</v-dropdown-item>
-        <v-dropdown-item>Test2</v-dropdown-item>
-        <v-dropdown-item>Test3</v-dropdown-item>
-      </ul>
-    </div>
+    <v-dropdown>
+      <template v-slot:button>
+        <v-dropdown-btn>
+          Size <span class="icon"><i class="fa fa-wrapper fa-caret-down"></i></span>
+        </v-dropdown-btn>
+      </template>
+      <v-dropdown-item>Test1</v-dropdown-item>
+      <v-dropdown-item>Test2</v-dropdown-item>
+      <v-dropdown-item>Test3</v-dropdown-item>
+    </v-dropdown>
   </v-container>
 </template>
