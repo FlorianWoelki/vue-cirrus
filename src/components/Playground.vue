4aa1f0cfc44d7c77cc53bc823ac586dca2b041b5
FlorianWoelki <FlorianWoelki@gmx.de>
2019-09-24 09:47:50 +0200

    added colors to playground btn

--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -2,17 +2,46 @@
   <v-container>
     <h3>Playground</h3>
     <v-btn center>Customize Me</v-btn>
-    <v-dropdown>
-      <template v-slot:button>
-        <v-dropdown-btn>
-          Size <span class="icon"><i class="fa fa-wrapper fa-caret-down"></i></span>
-        </v-dropdown-btn>
-      </template>
-      <v-dropdown-item>xSmall</v-dropdown-item>
-      <v-dropdown-item>Small</v-dropdown-item>
-      <v-dropdown-item>Normal</v-dropdown-item>
-      <v-dropdown-item>Large</v-dropdown-item>
-      <v-dropdown-item>xLarge</v-dropdown-item>
-    </v-dropdown>
+
+    <v-space />
+    <v-divider center short />
+    <v-space />
+
+    <v-row>
+      <v-col c4>
+        <v-dropdown>
+          <template v-slot:button>
+            <v-dropdown-btn>
+              Color <span class="icon"><i class="fa fa-wrapper fa-caret-down"></i></span>
+            </v-dropdown-btn>
+          </template>
+          <v-dropdown-item>Primary</v-dropdown-item>
+          <v-dropdown-item>Transparent</v-dropdown-item>
+          <v-dropdown-item>Light</v-dropdown-item>
+          <v-dropdown-item>Dark</v-dropdown-item>
+          <v-dropdown-item>Black</v-dropdown-item>
+          <v-dropdown-item>Info</v-dropdown-item>
+          <v-dropdown-item>Link</v-dropdown-item>
+          <v-dropdown-item>Success</v-dropdown-item>
+          <v-dropdown-item>Warning</v-dropdown-item>
+          <v-dropdown-item>Danger</v-dropdown-item>
+        </v-dropdown>
+        <v-checkbox>Outline</v-checkbox>
+      </v-col>
+      <v-col c4>
+        <v-dropdown>
+          <template v-slot:button>
+            <v-dropdown-btn>
+              Size <span class="icon"><i class="fa fa-wrapper fa-caret-down"></i></span>
+            </v-dropdown-btn>
+          </template>
+          <v-dropdown-item>xSmall</v-dropdown-item>
+          <v-dropdown-item>Small</v-dropdown-item>
+          <v-dropdown-item>Normal</v-dropdown-item>
+          <v-dropdown-item>Large</v-dropdown-item>
+          <v-dropdown-item>xLarge</v-dropdown-item>
+        </v-dropdown>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
