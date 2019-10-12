f69351181e19915047ccfe06988fd494e639c46f
FlorianWoelki <FlorianWoelki@gmx.de>
2019-10-12 12:08:49 +0200

    updated text for animated success buttons

--- src/views/Components/Buttons/Examples/BtnAnimated.vue
+++ src/views/Components/Buttons/Examples/BtnAnimated.vue
@@ -3,17 +3,17 @@
     <v-col c4>
       <v-btn center animated small>Normal</v-btn>
       <v-btn center animated primary small>Primary</v-btn>
-      <v-btn center animated success small>S</v-btn>
+      <v-btn center animated success small>Success</v-btn>
     </v-col>
     <v-col c4>
       <v-btn center animated>Normal</v-btn>
       <v-btn center animated primary>Primary</v-btn>
-      <v-btn center animated success>S</v-btn>
+      <v-btn center animated success>Success</v-btn>
     </v-col>
     <v-col c4>
       <v-btn center animated large>Normal</v-btn>
       <v-btn center animated primary large>Primary</v-btn>
-      <v-btn center animated success large>S</v-btn>
+      <v-btn center animated success large>Success</v-btn>
     </v-col>
   </v-row>
 </template>
