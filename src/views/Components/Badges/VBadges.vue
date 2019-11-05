13c4b3c6e86b1947451682364c6e31dc36d1b33c
florianwoelki <florianwoelki@gmx.de>
2019-11-05 19:27:07 +0100

    Add new badges view
    I've added the new view for any component for the badge component.

--- src/views/Components/Badges/VBadges.vue
+++ src/views/Components/Badges/VBadges.vue
@@ -1,65 +1,64 @@
 <template>
   <section class="badges-component">
-    <h5>Custom badges</h5>
-    <p>The badges functionality is a Vue Cirrus original and it is so easy to create one!</p>
-
-    <v-row>
-      <v-col c6>
-        <v-code lang="Vue">
-          <xmp v-html="code"></xmp>
-        </v-code>
-      </v-col>
-      <v-col
-        center
-        c6
-      >
-        <v-badge accent content="3">
-          <p>Paragraph Text</p>
-        </v-badge>
-      </v-col>
-    </v-row>
+    <p class="no-upper-margin">
+      You can use the <kbd>badges</kbd> to display text on any
+      component in the upper right corner. This is mostly used in notifications or messages.
+    </p>
 
     <v-space xlarge />
 
-    <h6>API (props)</h6>
-    <API :data="props" />
+    <h3>Usage</h3>
+    <p class="no-upper-margin">
+      Badges in their simples form display a bubble with text in it.
+    </p>
 
-    <v-space
-      xlarge
-      v-for="i in 2"
-      :key="i"
-    />
+    <v-space />
+    <Playground
+      noTitle
+      href="/#/components/Badges/#!"
+      :componentProps="propsData">
+    </Playground>
+
+    <v-space />
+    <h3 style="margin-bottom: 10px">API</h3>
+    <API :data="props" :dropdownItems="['v-badge']" />
+
+    <v-space xlarge />
+    <v-space xlarge />
   </section>
 </template>
 
 <script>
 import API from '@/views/Components/API.vue';
+import Playground from '@/components/Playground/Playground.vue';
 
 export default {
   components: {
     API,
+    Playground,
   },
 
   data() {
     return {
-      code: `<v-badge accent content="3">
-  <p>Paragraph Text</p>
-</v-badge>`,
-      props: {
-        content: ['content', 'Empty', 'String', 'Badge Text'],
-        left: ['left', 'false', 'Boolean', 'Left positioned badge'],
-        right: ['right', 'true', 'Boolean', 'Right positioned badge'],
-        bottom: ['bottom', 'false', 'Boolean', 'Bottom positioned badge'],
-        primary: ['primary', 'false', 'Boolean', 'Primary colored badge'],
-        info: ['info', 'false', 'Boolean', 'Info colored badge'],
-        link: ['link', 'false', 'Boolean', 'Link colored badge'],
-        dark: ['dark', 'false', 'Boolean', 'Dark colored badge'],
-        success: ['success', 'false', 'Boolean', 'Success colored badge'],
-        black: ['black', 'false', 'Boolean', 'Black colored badge'],
-        warning: ['warning', 'false', 'Boolean', 'Warning colored badge'],
-        danger: ['danger', 'false', 'Boolean', 'Danger colored badge'],
-        light: ['light', 'false', 'Boolean', 'Light colored badge'],
+      propsData: {
+        content: '',
+        position: '',
+        color: '',
       },
+      props: [
+        ['v-badge', 'content', 'string', '0', 'This is the content displayed in the badge.'],
+        ['v-badge', 'right', 'boolean', 'true', 'It will position the badge on the right of the component.'],
+        ['v-badge', 'left', 'boolean', 'false', 'It will position the badge on the left of the component.'],
+        ['v-badge', 'bottom', 'boolean', 'false', 'It will position the badge on the bottom of the component.'],
+        ['v-badge', 'primary', 'boolean', 'false', 'The badge will be the color: Primary.'],
+        ['v-badge', 'info', 'boolean', 'false', 'The badge will be the color: Info.'],
+        ['v-badge', 'link', 'boolean', 'false', 'The badge will be the color: Link.'],
+        ['v-badge', 'dark', 'boolean', 'false', 'The badge will be the color: Dark.'],
+        ['v-badge', 'light', 'boolean', 'false', 'The badge will be the color: Light.'],
+        ['v-badge', 'success', 'boolean', 'false', 'The badge will be the color: Success.'],
+        ['v-badge', 'warning', 'boolean', 'false', 'The badge will be the color: Warning.'],
+        ['v-badge', 'danger', 'boolean', 'false', 'The badge will be the color: Danger.'],
+      ],
     };
   },
 };
