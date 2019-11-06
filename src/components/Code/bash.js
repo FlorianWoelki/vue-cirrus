5fde5a7ba748cbbb84c1e10b4d73728615965f59
florianwoelki <florianwoelki@gmx.de>
2019-11-06 16:21:15 +0100

    Add keyword 'add' to bash code

--- src/components/Code/bash.js
+++ src/components/Code/bash.js
@@ -8,7 +8,7 @@ export default {
     class: 'string',
   },
   bashKeyword: {
-    exp: /\b(yarn|npm|install|echo|git|if|then|elif|fi|for|do|done|in|while|read|function|return|grep|pwd|case|printf|exit|shift)(?=[^\w])/g,
+    exp: /\b(yarn|npm|add|install|echo|git|if|then|elif|fi|for|do|done|in|while|read|function|return|grep|pwd|case|printf|exit|shift)(?=[^\w])/g,
     class: 'bash-keyword',
   },
 };
