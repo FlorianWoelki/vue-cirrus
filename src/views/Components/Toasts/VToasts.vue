6494d10784f32f8a2c4f471d3d36c44def5ad583
FlorianWoelki <FlorianWoelki@gmx.de>
2019-11-14 09:04:44 +0100

    Add toast code preview

--- src/views/Components/Toasts/VToasts.vue
+++ src/views/Components/Toasts/VToasts.vue
@@ -21,6 +21,7 @@
     >
       <template v-slot:component>
         <v-toast
+          disableCloseFunction
           :title="propsData.title"
           :closable="propsData.closable"
           :primary="propsData.color === 'primary'"
@@ -31,6 +32,49 @@
           :link="propsData.color === 'link'"
         >Customize Me</v-toast>
       </template>
+      <v-row>
+        <v-col c4 center>
+          <v-input-field
+            placeholder="Title ..."
+            v-model="propsData.title"
+          ></v-input-field>
+        </v-col>
+        <v-col c4 center>
+          <v-checkbox
+            @change="() => { propsData.closable = !propsData.closable }"
+          >Closable</v-checkbox>
+        </v-col>
+        <v-col c4 center>
+          <v-dropdown>
+            <template v-slot:button>
+              <v-dropdown-btn>
+                Color <span class="icon"><i class="fa fa-wrapper fa-caret-down"></i></span>
+              </v-dropdown-btn>
+            </template>
+            <v-dropdown-item
+              @click="(event) => { propsData.color = '' }"
+            >Normal</v-dropdown-item>
+            <v-dropdown-item
+              @click="(event) => { propsData.color = event.srcElement.innerHTML.toLowerCase() }"
+            >Primary</v-dropdown-item>
+            <v-dropdown-item
+              @click="(event) => { propsData.color = event.srcElement.innerHTML.toLowerCase() }"
+            >Success</v-dropdown-item>
+            <v-dropdown-item
+              @click="(event) => { propsData.color = event.srcElement.innerHTML.toLowerCase() }"
+            >Warning</v-dropdown-item>
+            <v-dropdown-item
+              @click="(event) => { propsData.color = event.srcElement.innerHTML.toLowerCase() }"
+            >Error</v-dropdown-item>
+            <v-dropdown-item
+              @click="(event) => { propsData.color = event.srcElement.innerHTML.toLowerCase() }"
+            >Info</v-dropdown-item>
+            <v-dropdown-item
+              @click="(event) => { propsData.color = event.srcElement.innerHTML.toLowerCase() }"
+            >Link</v-dropdown-item>
+          </v-dropdown>
+        </v-col>
+      </v-row>
     </Playground>
 
     <v-space xlarge />
