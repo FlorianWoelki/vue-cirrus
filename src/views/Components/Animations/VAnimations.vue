6ad7bff0d7c85f57324cda1ca87fb4f7a72d0802
florianwoelki <florianwoelki@gmx.de>
2019-11-18 16:35:09 +0100

    Add new animations view with playground

--- src/views/Components/Animations/VAnimations.vue
+++ src/views/Components/Animations/VAnimations.vue
@@ -1,139 +1,106 @@
 <template>
   <section class="animations-mixin">
-    <p>This mixin can almost applied to any Vue Cirrus component!</p>
-
-    <p>Short Demo</p>
-
-    <v-row>
-      <v-col
-        c4
-        center
-      >Fade In</v-col>
-      <v-col
-        c4
-        center
-      >Bounce In</v-col>
-      <v-col
-        c4
-        center
-        >Bounce</v-col>
-    </v-row>
-    <v-row>
-      <v-col
-        c4
-        center
-      >
-        <v-btn
-          fadeIn
-          infinite
-        >Btn</v-btn>
-      </v-col>
-      <v-col
-        c4
-        center
-      >
-        <v-btn
-          bounceIn
-          infinite
-        >Btn</v-btn>
-      </v-col>
-      <v-col
-        c4
-        center
-      >
-        <v-btn
-          bounce
-          infinite
-        >Btn</v-btn>
-      </v-col>
-    </v-row>
+    <p class="no-upper-margin">
+      You can almost apply those <kbd>animations</kbd> on every single vue-cirrus component.
+    </p>
 
     <v-space xlarge />
 
-    <v-row>
-      <v-col
-        c4
-        center
-      >Pulse</v-col>
-      <v-col
-        c4
-        center
-      >Hover Grow</v-col>
-      <v-col
-        c4
-        center
-      >Loading Left/Right</v-col>
-    </v-row>
-    <v-row>
-      <v-col
-        c4
-        center
-      >
+    <h3>Usage</h3>
+    <p class="no-upper-margin">
+      There are a some animations. Just try them out!
+    </p>
+
+    <v-space />
+    <Playground
+      noTitle
+      href="/#/components/Animations/#!"
+      component="v-btn"
+      :componentProps="propsData"
+    >
+      <template v-slot:component>
         <v-btn
-          pulse
-          infinite
-        >Btn</v-btn>
-      </v-col>
-      <v-col
-        c4
-        center
-      >
-        <v-btn
-          hoverGrow
-          infinite
-        >Btn</v-btn>
-      </v-col>
-      <v-col
-        c4
-        center
-      >
-        <v-btn
-          loadingLeft
-          infinite
-        >Btn</v-btn>
-      </v-col>
-    </v-row>
+          center
+          primary
+          :pulse="propsData.animation === 'pulse'"
+          :fadeIn="propsData.animation === 'fadeIn'"
+          :bounceIn="propsData.animation === 'bounceIn'"
+          :bounce="propsData.animation === 'bounce'"
+          :loadingLeft="propsData.animation === 'loadingLeft'"
+          :loadingRight="propsData.animation === 'loadingRight'"
+          :hoverGrow="propsData.animation === 'hoverGrow'"
+          :infinite="propsData.infinite"
+        >Customize Me</v-btn>
+      </template>
+      <v-row>
+        <v-col c12 center>
+          <v-dropdown>
+            <template v-slot:button>
+              <v-dropdown-btn>
+                Animation <span class="icon"><i class="fa fa-wrapper fa-caret-down"></i></span>
+              </v-dropdown-btn>
+            </template>
+            <v-dropdown-item
+              @click="(event) => { propsData.animation = 'fadeIn' }"
+            >FadeIn</v-dropdown-item>
+            <v-dropdown-item
+              @click="(event) => { propsData.animation = 'bounceIn' }"
+            >BounceIn</v-dropdown-item>
+            <v-dropdown-item
+              @click="(event) => { propsData.animation = event.srcElement.innerHTML.toLowerCase() }"
+            >Bounce</v-dropdown-item>
+            <v-dropdown-item
+              @click="(event) => { propsData.animation = event.srcElement.innerHTML.toLowerCase() }"
+            >Pulse</v-dropdown-item>
+            <v-dropdown-item
+              @click="(event) => { propsData.animation = 'hoverGrow' }"
+            >HoverGrow</v-dropdown-item>
+            <v-dropdown-item
+              @click="(event) => { propsData.animation = 'loadingLeft' }"
+            >LoadingLeft</v-dropdown-item>
+            <v-dropdown-item
+              @click="(event) => { propsData.animation = 'loadingRight' }"
+            >LoadingRight</v-dropdown-item>
+          </v-dropdown>
+          <v-checkbox
+            @change="() => { propsData.infinite = !propsData.infinite }"
+          >Infinite</v-checkbox>
+        </v-col>
+      </v-row>
+    </Playground>
 
     <v-space xlarge />
-
-    <v-row>
-      <span>Code example</span>
-      <v-code lang="Vue">
-        <xmp v-html=code></xmp>
-      </v-code>
-    </v-row>
-
-    <v-space xlarge />
-
-    <h6>API (props)</h6>
-    <API :data="props" hideMixins=true />
+    <h3 style="margin-bottom: 10px">API</h3>
+    <API :data="props" :dropdownItems="['animation']" />
   </section>
 </template>
 
 <script>
 import API from '../API.vue';
+import Playground from '@/components/Playground/Playground.vue';
 
 export default {
   components: {
     API,
+    Playground,
   },
 
   data() {
     return {
-      code: `<v-btn
-  pulse
-  infinite
->Btn</v-btn>`,
-      props: {
-        infinite: ['infinite', 'false', 'Boolean', 'Animation will be infinite'],
-        fadeIn: ['fadeIn', 'false', 'Boolean', 'Enable fade in animation'],
-        bounceIn: ['bounceIn', 'false', 'Boolean', 'Enable bounce in animation'],
-        bounce: ['bounce', 'false', 'Boolean', 'Enable bounce animation'],
-        pulse: ['pulse', 'false', 'Boolean', 'Enable pulse animation'],
-        hoverGrow: ['hoverGrow', 'false', 'Boolean', 'Enable hover grow animation'],
-        loadingLeft: ['loadingLeft', 'false', 'Boolean', 'Enable loading left animation'],
-        loadingRight: ['loadingRight', 'false', 'Boolean', 'Enable loading right animation'],
+      propsData: {
+        infinite: false,
+        animation: '',
       },
+      props: [
+        ['animation', 'infinite', 'boolean', 'false', 'The specified animation will be infinite.'],
+        ['animation', 'fadeIn', 'boolean', 'false', 'Enable fade in animation. It will go from 0 opacity to 100 opacity.'],
+        ['animation', 'bounceIn', 'boolean', 'false', 'Bounces in like a ball animation.'],
+        ['animation', 'bounce', 'boolean', 'false', 'Bounces like a ball animation.'],
+        ['animation', 'pulse', 'boolean', 'false', 'Heart pulsing animation'],
+        ['animation', 'hoverGrow', 'boolean', 'false', 'When a users hovers on the component it will grow.'],
+        ['animation', 'loadingLeft', 'boolean', 'false', 'Enable the loading circle on the left hand side.'],
+        ['animation', 'loadingRight', 'boolean', 'false', 'Enable the loading circle on the right hand side.'],
+      ],
     };
   },
 };
