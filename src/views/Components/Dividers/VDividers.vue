96219f6f9fb231a255d2f6d7bbb56f77217890af
FlorianWoelki <FlorianWoelki@gmx.de>
2019-11-19 14:52:49 +0100

    Add new dividers view

--- src/views/Components/Dividers/VDividers.vue
+++ src/views/Components/Dividers/VDividers.vue
@@ -1,55 +1,59 @@
 <template>
-  <section class="dividers-component">
-    <h5>Dividers</h5>
-    <p>Easily split up your layout with dividers!</p>
-
-    <v-row>
-      <v-col c6>
-        <v-code lang="Vue">
-          <xmp v-html="code"></xmp>
-        </v-code>
-      </v-col>
-      <v-col
-        center
-        c6
-      >
-        <p>Paragraph Text 1</p>
-        <v-divider />
-        <p>Paragraph Text 2</p>
-      </v-col>
-    </v-row>
+  <section class="animations-mixin">
+    <p class="no-upper-margin">
+      You can use the <kbd>v-divider</kbd> component to seperate content from other content.
+    </p>
 
     <v-space xlarge />
 
-    <h6>API (props)</h6>
-    <API :data="props" />
+    <h3>Usage</h3>
+    <p class="no-upper-margin">
+      Default dividers are horizontal and long.
+    </p>
 
-    <v-space
-      xlarge
-      v-for="i in 2"
-      :key="i"
-    />
+    <v-space />
+    <Playground
+      noTitle
+      href="/#/components/Dividers/#!"
+      component="v-divider"
+      :componentProps="propsData"
+      disableMessage
+    >
+      <template v-slot:component>
+        <v-divider></v-divider>
+      </template>
+      <v-row>
+      </v-row>
+    </Playground>
+
+    <v-space xlarge />
+    <h3 style="margin-bottom: 10px">API</h3>
+    <API :data="props" :dropdownItems="['v-divider']" />
   </section>
 </template>
 
 <script>
-import API from '@/views/Components/API.vue';
+import API from '../API.vue';
+import Playground from '@/components/Playground/Playground.vue';
 
 export default {
   components: {
     API,
+    Playground,
   },
 
   data() {
     return {
-      code: `<p>Paragraph Text 1</p>
-<v-divider />
-<p>Paragraph Text 2</p>`,
-      props: {
-        short: ['short', 'false', 'Boolean', 'Short divider'],
-        vertical: ['vertical', 'false', 'Boolean', 'Vertical Divider with optional content'],
-        content: ['content', 'Empty', 'String', 'Content for the vertical divider'],
+      propsData: {
+        short: false,
+        vertical: false,
+        content: '',
       },
+      props: [
+        ['v-divider', 'short', 'boolean', 'false', 'Set the divider size to short.'],
+        ['v-divider', 'vertical', 'boolean', 'false', 'Enable a vertical divider.'],
+        ['v-divider', 'content', 'string', 'empty', 'Set the content of the divider for the vertical one.'],
+      ],
     };
   },
 };
