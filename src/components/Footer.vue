538cc674a34ebbb8301981878af29ed951fe6a03
florianwoelki <florianwoelki@gmx.de>
2019-11-21 15:21:06 +0100

    Add footer links to components

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,6 +12,35 @@
       Made with <i class="fa fa-heart animated pulse" aria-hidden="true"></i>
       by <span class="white"><v-link href="http://florianwoelki.com" blank>Florian Woelki</v-link></span>
     </p>
+
+    <v-container>
+      <v-row center>
+        <v-col c3>
+          <v-link href="#/components/Avatars">Avatars</v-link>
+          <v-link href="#/components/Badges">Badges</v-link>
+          <v-link href="#/components/Buttons">Buttons</v-link>
+          <v-link href="#/components/Cards">Cards</v-link>
+        </v-col>
+        <v-col c3>
+          <v-link href="#/components/Code">Code</v-link>
+          <v-link href="#/components/Forms">Forms</v-link>
+          <v-link href="#/components/Footer">Footer</v-link>
+          <v-link href="#/components/Grid">Grid</v-link>
+        </v-col>
+        <v-col c3>
+          <v-link href="#/components/Modals">Modals</v-link>
+          <v-link href="#/components/Pagination">Pagination</v-link>
+          <v-link href="#/components/Snackbars">Snackbars</v-link>
+          <v-link href="#/components/Tabs">Tabs</v-link>
+        </v-col>
+        <v-col c3>
+          <v-link href="#/components/Tags">Tags</v-link>
+          <v-link href="#/components/Toasts">Toasts</v-link>
+          <v-link href="#/components/Animations">Animations</v-link>
+          <v-link href="#/components/Tooltips">Tooltips</v-link>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-footer>
 </template>
 
