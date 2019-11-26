b0445de5f3737fef97ea96098f7ab055512e9349
FlorianWoelki <FlorianWoelki@gmx.de>
2019-11-26 09:07:41 +0100

    Add spacing between links and content

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,13 +5,21 @@
     <p>
       Code is licensed with
       <v-link href="https://opensource.org/licenses/MIT" blank>MIT</v-link>.
-      Everything is <span class="white">free</span> and <v-link href="https://github.com/FlorianWoelki/vue-cirrus" blank>open source</v-link>!
+      Everything is
+      <span class="white">free</span> and
+      <v-link href="https://github.com/FlorianWoelki/vue-cirrus" blank>open source</v-link>!
     </p>
 
     <p>
-      Made with <i class="fa fa-heart animated pulse" aria-hidden="true"></i>
-      by <span class="white"><v-link href="http://florianwoelki.com" blank>Florian Woelki</v-link></span>
+      Made with
+      <i class="fa fa-heart animated pulse" aria-hidden="true"></i>
+      by
+      <span class="white">
+        <v-link href="http://florianwoelki.com" blank>Florian Woelki</v-link>
+      </span>
     </p>
+
+    <v-space></v-space>
 
     <v-container>
       <v-row>
@@ -47,7 +55,7 @@
 <style lang="scss">
 .footer-section {
   .fa-heart {
-    color: #E90606;
+    color: #e90606;
     margin: 0 3px;
     font-size: 16px;
   }
