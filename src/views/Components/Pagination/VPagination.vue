cd27188d043881913d1cc5cc43582f3c2fdac31f
FlorianWoelki <FlorianWoelki@gmx.de>
2019-11-28 13:52:20 +0100

    Add new pagination component view

--- src/views/Components/Pagination/VPagination.vue
+++ src/views/Components/Pagination/VPagination.vue
@@ -1,62 +1,96 @@
 <template>
   <section class="pagination-component">
-    <h5>Pagination</h5>
-    <p>There is an easy pagination component in Vue Cirrus.</p>
-
-    <v-row>
-      <v-col c6>
-        <v-code lang="Vue">
-          <xmp v-html="code"></xmp>
-        </v-code>
-      </v-col>
-      <v-col
-        center
-        c6
-      >
-        <v-pagination bordered>
-          <v-pagination-item disabled>Prev</v-pagination-item>
-          <v-pagination-item selected>1</v-pagination-item>
-          <v-pagination-item>2</v-pagination-item>
-          <v-pagination-item>Next</v-pagination-item>
-        </v-pagination>
-      </v-col>
-    </v-row>
+    <p class="no-upper-margin">
+      Simply use the <kbd>v-pagination</kbd> component to create easy pagination
+      with just a few lines of code.
+    </p>
 
     <v-space xlarge />
 
-    <h6>API (props)</h6>
-    <API :data="props" />
+    <h3>Usage</h3>
+    <p class="no-upper-margin">
+      Pagination in their simples form have no border and a hover color.
+    </p>
 
-    <v-space
-      xlarge
-      v-for="i in 2"
-      :key="i"
-    />
+    <v-space />
+    <Playground
+      noTitle
+      href="/#/components/Pagination/#!"
+      component="v-pagination"
+      :componentProps="propsData"
+      :customCode="customCode"
+    >
+      <template v-slot:component>
+        <div class="u-center">
+          <v-pagination
+            :bordered="propsData.bordered"
+          >
+            <v-pagination-item disabled>Prev</v-pagination-item>
+            <v-pagination-item selected>1</v-pagination-item>
+            <v-pagination-item>2</v-pagination-item>
+            <v-pagination-item>3</v-pagination-item>
+            <v-pagination-item>Next</v-pagination-item>
+          </v-pagination>
+        </div>
+      </template>
+      <v-row>
+        <v-col center c12>
+          <v-checkbox
+            @change="() => { propsData.bordered = !propsData.bordered }"
+          >Bordered Pagination</v-checkbox>
+        </v-col>
+      </v-row>
+    </Playground>
+
+    <v-space xlarge />
+    <h3 style="margin-bottom: 10px">API</h3>
+    <API :data="props" :dropdownItems="['v-pagination', 'v-pagination-item']" />
+
+    <v-space xlarge />
+    <v-space xlarge />
+    <h3>Examples</h3>
   </section>
 </template>
 
 <script>
 import API from '@/views/Components/API.vue';
+import Playground from '@/components/Playground/Playground.vue';
 
 export default {
   components: {
     API,
+    Playground,
   },
 
   data() {
     return {
-      code: `<v-pagination bordered>
+      propsData: {
+        bordered: false,
+        xlarge: false,
+      },
+      props: [
+        ['v-pagination', 'bordered', 'boolean', 'false', 'Activate the border around the pagination.'],
+        ['v-pagination-item', 'selected', 'boolean', 'false', 'Set the pagination item in a selected red state.'],
+        ['v-pagination-item', 'disabled', 'boolean', 'false', 'Set the pagination item in a disabled state.'],
+        ['v-pagination-item', 'href', 'string', '#', 'Set the href, when someone clicks the pagination item.'],
+        ['v-pagination-item', '@click', 'function', '() => {}', 'Get the event, when someone clicks the pagination item.'],
+      ],
+    };
+  },
+
+
+  computed: {
+    customCode() {
+      return `
+<v-pagination${this.propsData.bordered ? ' bordered' : ''}>
   <v-pagination-item disabled>Prev</v-pagination-item>
   <v-pagination-item selected>1</v-pagination-item>
   <v-pagination-item>2</v-pagination-item>
+  <v-pagination-item>3</v-pagination-item>
   <v-pagination-item>Next</v-pagination-item>
-</v-pagination>`,
-      props: {
-        link: ['link', '#', 'String', 'PaginationItem href'],
-        selected: ['selected', 'false', 'Boolean', 'PaginationItem is selected'],
-        disabled: ['disabled', 'false', 'Boolean', 'PaginationItem is disabled'],
-      },
-    };
+</v-pagination>
+`;
+    },
   },
 };
 </script>
