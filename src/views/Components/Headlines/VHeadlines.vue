41946bd10b340f4148cfdb4125e2d0ae69a7f45c
florianwoelki <florianwoelki@gmx.de>
2019-11-29 10:37:23 +0100

    Opimize view of headline component

--- src/views/Components/Headlines/VHeadlines.vue
+++ src/views/Components/Headlines/VHeadlines.vue
@@ -1,66 +1,104 @@
 <template>
   <section class="headlines-component">
-    <h5>Make it bigger</h5>
-    <p>Create really huge headlines with this component.</p>
-
-    <v-row>
-      <v-col c6>
-        <v-code
-          lang="Vue"
-          :dark="isDarkMode"
-        >
-          <xmp v-html="code"></xmp>
-        </v-code>
-      </v-col>
-      <v-col
-        center
-        c6
-        style="margin-top: 30px;"
-      >
-        <v-headline uppercase h1>HEADLINE1</v-headline>
-        <v-headline uppercase h2>HEADLINE2</v-headline>
-        <v-headline uppercase h3>HEADLINE3</v-headline>
-        <v-headline uppercase h4>HEADLINE4</v-headline>
-      </v-col>
-    </v-row>
+    <p class="no-upper-margin">
+      Headlines are bigger than the normal html <kbd>h</kbd> tags.
+      <kbd>v-headline</kbd> is a simple component to create big stunning
+      Headlines.
+    </p>
 
     <v-space xlarge />
 
-    <h6>API (props)</h6>
-    <API :data="props" />
+    <h3>Usage</h3>
+    <p class="no-upper-margin">
+      Tabs are so clean and easy to see by their default appearance.
+    </p>
 
-    <v-space
-      xlarge
-      v-for="i in 2"
-      :key="i"
-    />
+    <v-space />
+    <Playground
+      noTitle
+      href="/#/components/Headlines/#!"
+      component="v-headline"
+      :componentProps="propsData"
+    >
+      <template v-slot:component>
+        <v-headline
+          class="u-text-center"
+          :h1="propsData.h1"
+          :h2="propsData.h2"
+          :h3="propsData.h3"
+          :h4="propsData.h4"
+          :uppercase="propsData.uppercase"
+        >
+          Customize Me
+        </v-headline>
+      </template>
+      <v-row>
+        <v-col c6 center>
+          <v-checkbox
+            @change="() => { propsData.uppercase = !propsData.uppercase }"
+          >Uppercase</v-checkbox>
+        </v-col>
+        <v-col c6 center>
+          <v-dropdown>
+            <template v-slot:button>
+              <v-dropdown-btn>
+                Size <span class="icon"><i class="fa fa-wrapper fa-caret-down"></i></span>
+              </v-dropdown-btn>
+            </template>
+            <v-dropdown-item
+              @click="(event) => { propsData.size = '' }"
+            >Normal</v-dropdown-item>
+            <v-dropdown-item
+              @click="(event) => { propsData.size = event.srcElement.innerHTML.toLowerCase() }"
+            >H1</v-dropdown-item>
+            <v-dropdown-item
+              @click="(event) => { propsData.size = event.srcElement.innerHTML.toLowerCase() }"
+            >H2</v-dropdown-item>
+            <v-dropdown-item
+              @click="(event) => { propsData.size = event.srcElement.innerHTML.toLowerCase() }"
+            >H3</v-dropdown-item>
+            <v-dropdown-item
+              @click="(event) => { propsData.size = event.srcElement.innerHTML.toLowerCase() }"
+            >H4</v-dropdown-item>
+          </v-dropdown>
+        </v-col>
+      </v-row>
+    </Playground>
+
+    <v-space xlarge />
+    <h3 style="margin-bottom: 10px">API</h3>
+    <API :data="props" :dropdownItems="['v-headline']" />
+
+    <v-space xlarge />
+    <v-space xlarge />
+    <h3>Examples</h3>
   </section>
 </template>
 
 <script>
 import API from '@/views/Components/API.vue';
+import Playground from '@/components/Playground/Playground.vue';
 
 export default {
   components: {
     API,
+    Playground,
   },
 
   data() {
     return {
-      code: `<v-headline uppercase h1>HEADLINE1</v-headline>
-<v-headline uppercase h2>HEADLINE2</v-headline>
-<v-headline uppercase h3>HEADLINE3</v-headline>
-<v-headline uppercase h4>HEADLINE4</v-headline>`,
-      props: {
-        uppercase: ['uppercase', 'false', 'Boolean', 'Make the headline uppercase'],
+      propsData: {
+        size: '',
+        uppercase: false,
       },
+      props: [
+        ['v-headline', 'h1', 'boolean', 'false', 'Set the headline size to 6.5rem.'],
+        ['v-headline', 'h2', 'boolean', 'false', 'Set the headline size to 5.5rem.'],
+        ['v-headline', 'h3', 'boolean', 'false', 'Set the headline size to 4.5rem.'],
+        ['v-headline', 'h4', 'boolean', 'false', 'Set the headline size to 3.5rem.'],
+        ['v-headline', 'uppercase', 'boolean', 'false', 'Set the headline to uppercase.'],
+      ],
     };
-  },
-
-  computed: {
-    isDarkMode() {
-      return this.$store.getters.isDarkMode.darkMode;
-    },
   },
 };
 </script>
