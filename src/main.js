21d1619e54f69be892ae8cb15aa03bab6b97b806
florianwoelki <florianwoelki@gmx.de>
2019-12-02 09:30:38 +0100

    Drop not useful comments

--- src/main.js
+++ src/main.js
@@ -1,15 +1,3 @@
-/*
-import Vue from 'vue';
-import './components/_globals';
-import App from './App.vue';
-
-Vue.config.productionTip = false;
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app');
-*/
-
 const install = (Vue) => {
   const requireComponent = require.context('./components', true, /V[\w-]+.vue$/);
 
