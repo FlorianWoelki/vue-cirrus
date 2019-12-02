eb9ea5c584b1d9afaae75a4cf1881d32b6e1d5e1
florianwoelki <florianwoelki@gmx.de>
2019-12-02 19:40:55 +0100

    Optimize button color and size props

--- src/components/Btn/VBtn.vue
+++ src/components/Btn/VBtn.vue
@@ -1,6 +1,17 @@
 <template>
   <button
-    :class="classes"
+    :class="[
+      this.tooltipMixins,
+      this.animationsMixins,
+      this.color ? `btn-${this.color}` : null,
+      this.size ? `btn-${this.size}` : null,
+      {
+        'btn-animated': this.animated,
+        'outline': this.outline,
+        'btn--pilled': this.pilled,
+        'btn--circle': this.circle,
+      },
+    ]"
     :data-tooltip="tooltipData"
     :disabled="disabled"
     @click="handleClick($event)"
@@ -32,62 +43,6 @@ export default {
       type: Boolean,
       default: false,
     },
-    primary: {
-      type: Boolean,
-      default: false,
-    },
-    transparent: {
-      type: Boolean,
-      default: false,
-    },
-    light: {
-      type: Boolean,
-      default: false,
-    },
-    dark: {
-      type: Boolean,
-      default: false,
-    },
-    black: {
-      type: Boolean,
-      default: false,
-    },
-    info: {
-      type: Boolean,
-      default: false,
-    },
-    link: {
-      type: Boolean,
-      default: false,
-    },
-    success: {
-      type: Boolean,
-      default: false,
-    },
-    warning: {
-      type: Boolean,
-      default: false,
-    },
-    danger: {
-      type: Boolean,
-      default: false,
-    },
-    tiny: {
-      type: Boolean,
-      default: false,
-    },
-    small: {
-      type: Boolean,
-      default: false,
-    },
-    large: {
-      type: Boolean,
-      default: false,
-    },
-    xlarge: {
-      type: Boolean,
-      default: false,
-    },
     outline: {
       type: Boolean,
       default: false,
@@ -96,6 +51,14 @@ export default {
       type: Boolean,
       default: false,
     },
+    color: {
+      type: String,
+      default: null,
+    },
+    size: {
+      type: String,
+      default: null,
+    },
   },
 
   methods: {
@@ -103,37 +66,6 @@ export default {
       if (!this.disabled) {
         this.$emit('click', event);
       }
-    },
-  },
-
-  computed: {
-    classes() {
-      const classes = {
-        'btn-animated': this.animated,
-        'btn-primary': this.primary,
-        'btn-transparent': this.transparent,
-        'btn-light': this.light,
-        'btn-dark': this.dark,
-        'btn-black': this.black,
-        'btn-info': this.info,
-        'btn-link': this.link,
-        'btn-success': this.success,
-        'btn-warning': this.warning,
-        'btn-danger': this.danger,
-        'btn-tiny': this.tiny,
-        'btn-small': this.small,
-        'btn-large': this.large,
-        'btn-xlarge': this.xlarge,
-        outline: this.outline,
-        'btn--pilled': this.pilled,
-        'btn--circle': this.circle,
-      };
-
-      return Object.assign(
-        this.tooltipMixins,
-        this.animationsMixins,
-        classes,
-      );
     },
   },
 };
