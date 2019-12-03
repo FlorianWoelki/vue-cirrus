3efb6592138834ee3801584874775f69b008df2a
FlorianWoelki <FlorianWoelki@gmx.de>
2019-12-03 09:18:16 +0100

    Optimize color prop and html structure of toast component

--- src/components/VToast.vue
+++ src/components/VToast.vue
@@ -1,19 +1,18 @@
 <template>
   <div
-    :class="classes"
+    :class="[
+      this.tooltipMixins,
+      this.animationsMixins,
+      'toast',
+      `toast--${this.color}`,
+    ]"
     :data-tooltip="tooltipData"
   >
     <button
-      v-if="closable && !disableCloseFunction"
+      v-if="closable"
       class="btn-close"
       @click="handleClose($event)"
     ></button>
-    <button
-      v-else-if="closable"
-      class="btn-close"
-      @click="handleClick($event)"
-    >
-    </button>
     <h4 v-if="title !== ''" class="toast__title">{{ title }}</h4>
     <slot></slot>
   </div>
@@ -38,29 +37,9 @@ export default {
       type: String,
       default: '',
     },
-    success: {
-      type: Boolean,
-      default: false,
-    },
-    warning: {
-      type: Boolean,
-      default: false,
-    },
-    error: {
-      type: Boolean,
-      default: false,
-    },
-    info: {
-      type: Boolean,
-      default: false,
-    },
-    link: {
-      type: Boolean,
-      default: false,
-    },
-    primary: {
-      type: Boolean,
-      default: false,
+    color: {
+      type: String,
+      default: null,
     },
     closable: {
       type: Boolean,
@@ -73,26 +52,10 @@ export default {
       this.$emit('click', event);
     },
     handleClose(event) {
-      const buttonElement = event.srcElement;
-      buttonElement.parentNode.style.visibility = 'hidden';
-    },
-  },
-
-  computed: {
-    classes() {
-      return Object.assign(
-        this.tooltipMixins,
-        this.animationsMixins,
-        {
-          toast: true,
-          'toast--success': this.success,
-          'toast--warning': this.warning,
-          'toast--error': this.error,
-          'toast--info': this.info,
-          'toast--link': this.link,
-          'toast--primary': this.primary,
-        },
-      );
+      if (!this.disableCloseFunction) {
+        const buttonElement = event.srcElement;
+        buttonElement.parentNode.style.visibility = 'hidden';
+      }
     },
   },
 };
