a071bcc5573a7e8e955ab4dcee40913dd5f6d395
florianwoelki <florianwoelki@gmx.de>
2019-12-06 11:34:00 +0100

    Fix badge view component

--- src/views/Components/Badges/Examples/BadgeColors.vue
+++ src/views/Components/Badges/Examples/BadgeColors.vue
@@ -1,47 +1,47 @@
 <template>
-  <v-row center class="badge-example-section">
-    <v-col c4>
+  <v-row class="badge-example-section">
+    <v-col c="4">
       <div class="u-center">
-        <v-badge primary content="20">
+        <v-badge color="primary" content="20">
           <p>Primary</p>
         </v-badge>
       </div>
       <div class="u-center">
-        <v-badge info content="21">
+        <v-badge color="info" content="21">
           <p>Info</p>
         </v-badge>
       </div>
       <div class="u-center">
-        <v-badge link content="22">
+        <v-badge color="link" content="22">
           <p>Link</p>
         </v-badge>
       </div>
     </v-col>
-    <v-col c4>
+    <v-col c="4">
       <div class="u-center">
-        <v-badge dark content="23">
+        <v-badge color="dark" content="23">
           <p>Dark</p>
         </v-badge>
       </div>
       <div class="u-center">
-        <v-badge light content="24">
+        <v-badge color="light" content="24">
           <p>Light</p>
         </v-badge>
       </div>
     </v-col>
-    <v-col c4>
+    <v-col c="4">
       <div class="u-center">
-        <v-badge success content="25">
+        <v-badge color="success" content="25">
           <p>Success</p>
         </v-badge>
       </div>
       <div class="u-center">
-        <v-badge warning content="26">
+        <v-badge color="warning" content="26">
           <p>Warning</p>
         </v-badge>
       </div>
       <div class="u-center">
-        <v-badge danger content="27">
+        <v-badge color="danger" content="27">
           <p>Danger</p>
         </v-badge>
       </div>
