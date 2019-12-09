f4a3a3b69c716124e7bf6ee5b79379f58814fff3
florianwoelki <florianwoelki@gmx.de>
2019-12-09 17:09:39 +0100

    Remove not useful div in badge alignment example

--- src/views/Components/Badges/Examples/BadgeAlignment.vue
+++ src/views/Components/Badges/Examples/BadgeAlignment.vue
@@ -1,18 +1,14 @@
 <template>
-  <v-row center class="badge-example-section">
-    <v-col c="6">
-      <div class="u-center">
-        <v-badge right content="11">
-          <p>Right</p>
-        </v-badge>
-      </div>
+  <v-row class="badge-example-section">
+    <v-col c="6" class="u-center">
+      <v-badge position="right" content="11">
+        <p>Right</p>
+      </v-badge>
     </v-col>
-    <v-col c="6">
-      <div class="u-center">
-        <v-badge position="left" content="13">
-          <p>Left</p>
-        </v-badge>
-      </div>
+    <v-col c="6" class="u-center">
+      <v-badge position="left" content="13">
+        <p>Left</p>
+      </v-badge>
     </v-col>
   </v-row>
 </template>
