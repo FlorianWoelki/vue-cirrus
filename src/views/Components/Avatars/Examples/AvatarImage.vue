68fbdaf5e08fd34ff787b942101e59da8dca6b38
florianwoelki <florianwoelki@gmx.de>
2019-12-09 17:08:14 +0100

    Remove not useful props for avatar images

--- src/views/Components/Avatars/Examples/AvatarImage.vue
+++ src/views/Components/Avatars/Examples/AvatarImage.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-row center class="avatar-example-size">
+  <v-row class="avatar-example-size u-text-center">
     <v-col c="3">
       <v-avatar xsmall src="https://randomuser.me/api/portraits/men/30.jpg" />
     </v-col>
