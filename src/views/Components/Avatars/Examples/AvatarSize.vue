345aa1fb579749b4663bd7770a7fcbbbd78a169b
florianwoelki <florianwoelki@gmx.de>
2019-12-09 17:07:34 +0100

    Add size information to avatar size example

--- src/views/Components/Avatars/Examples/AvatarSize.vue
+++ src/views/Components/Avatars/Examples/AvatarSize.vue
@@ -1,16 +1,20 @@
 <template>
-  <v-row center class="avatar-example-size">
+  <v-row level class="avatar-example-size u-text-center">
     <v-col c="3">
       <v-avatar xsmall text="FW" />
+      <span class="font-thin">xsmall</span>
     </v-col>
     <v-col c="3">
       <v-avatar small text="FW" />
+      <span class="font-thin">small</span>
     </v-col>
     <v-col c="3">
       <v-avatar large text="FW" />
+      <span class="font-thin">large</span>
     </v-col>
     <v-col c="3">
       <v-avatar xlarge text="FW" />
+      <span class="font-thin">xlarge</span>
     </v-col>
   </v-row>
 </template>
