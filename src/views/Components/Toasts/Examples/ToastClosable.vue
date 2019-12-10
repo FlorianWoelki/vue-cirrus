1b7e7ce42652c4fd5a95178b70777ce77374d016
FlorianWoelki <FlorianWoelki@gmx.de>
2019-12-10 13:08:49 +0100

    Fix column and name of the toast issue in toast closable example

--- src/views/Components/Toasts/Examples/ToastClosable.vue
+++ src/views/Components/Toasts/Examples/ToastClosable.vue
@@ -1,13 +1,13 @@
 <template>
   <v-row class="u-center toast-example-section">
-    <v-col c4>
+    <v-col c="4">
       <v-toast color="primary" closable disableCloseFunction>Primary Color</v-toast>
     </v-col>
-    <v-col c4>
-      <v-toast color="info" closable disableCloseFunction>Primary Color</v-toast>
+    <v-col c="4">
+      <v-toast color="info" closable disableCloseFunction>Info Color</v-toast>
     </v-col>
-    <v-col c4>
-      <v-toast color="link" closable disableCloseFunction>Primary Color</v-toast>
+    <v-col c="4">
+      <v-toast color="link" closable disableCloseFunction>Link Color</v-toast>
     </v-col>
   </v-row>
 </template>
