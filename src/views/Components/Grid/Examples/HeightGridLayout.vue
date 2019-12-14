810122e619594cdd942851ff438e062cc8af995f
florianwoelki <florianwoelki@gmx.de>
2019-12-14 12:10:28 +0100

    Add height grid layout example

---
+++ src/views/Components/Grid/Examples/HeightGridLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="grid-example-height u-text-center">
+    <v-row style="height: 300px;">
+      <v-col
+        v-for="i in range()"
+        :key="i"
+        :h="i"
+        fluid
+        class="column-showcase"
+      >h="{{ i }}"</v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    range() {
+      const result = [];
+      for (let i = 10; i <= 100; i += 10) {
+        result.push(`${i}`);
+      }
+      return result;
+    },
+  },
+};
+</script>
