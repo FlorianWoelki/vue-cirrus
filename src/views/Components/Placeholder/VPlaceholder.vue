d2983575b9fa00c3bc7073c57fb64daff9a7966d
FlorianWoelki <FlorianWoelki@gmx.de>
2019-12-16 19:39:18 +0100

    Change placeholder component to better and live component

--- src/views/Components/Placeholder/VPlaceholder.vue
+++ src/views/Components/Placeholder/VPlaceholder.vue
@@ -1,11 +1,74 @@
 <template>
-  <section class="placeholder-component">
-    <h1 class="u-text-center">Coming soon</h1>
-    <p class="font-thin u-text-center">This site will become perfect really soon.</p>
+  <section class="frames-component">
+    <p class="no-upper-margin">
+      You can use the <kbd>v-placeholder</kbd> component to create
+      simple, but powerful placeholders.
+    </p>
+
+    <v-space xlarge />
+
+    <h3>Usage</h3>
+    <p class="no-upper-margin">
+      The simplest placeholder has a standard title, subtitle and no icon.
+    </p>
+
+    <v-space />
+    <Playground
+      noTitle
+      href="/#/components/Placeholder/#!"
+      component="v-placeholder"
+      :componentProps="propsData"
+    >
+      <template v-slot:component>
+        <v-placeholder :title="propsData.title" :subtitle="propsData.subtitle">
+        </v-placeholder>
+      </template>
+      <v-row>
+        <v-col c="6" class="u-center">
+          <v-input-field
+            placeholder="Title ..."
+            v-model="propsData.title"
+          ></v-input-field>
+        </v-col>
+        <v-col c="6" class="u-center">
+          <v-input-field
+            placeholder="Subtitle ..."
+            v-model="propsData.subtitle"
+          ></v-input-field>
+        </v-col>
+      </v-row>
+    </Playground>
+
+    <v-space xlarge />
+    <h3 style="margin-bottom: 10px">API</h3>
+    <API :data="props" :dropdownItems="['v-placeholder', 'v-placeholder-item']" />
+
+    <v-space xlarge />
+    <v-space xlarge />
+    <h3>Examples</h3>
   </section>
 </template>
 
 <script>
+import API from '@/views/Components/API.vue';
+import Playground from '@/components/Playground/Playground.vue';
+
 export default {
+  components: {
+    API,
+    Playground,
+  },
+
+  data() {
+    return {
+      propsData: {
+        title: 'Coming Soon',
+        subtitle: 'We are currently working on this segment! :)',
+      },
+      props: [
+        ['v-frame', 'height', 'string', '30rem', 'Set the height for the frame.'],
+      ],
+    };
+  },
 };
 </script>
