8bc1825c7b13de163720734dccd2f9d382f58346
FlorianWoelki <FlorianWoelki@gmx.de>
2019-12-18 10:23:35 +0100

    Add dropdown menus for modifying props

--- src/views/Components/Modals/VModals.vue
+++ src/views/Components/Modals/VModals.vue
@@ -24,10 +24,56 @@
         <a href="#modal">
           <v-btn class="u-center" color="success">Open Modal</v-btn>
         </a>
-        <v-modal zoomIn large>
-          <p>testing</p>
+        <v-modal
+          :zoomIn="propsData.animation === 'zoomIn'"
+          :zoomOut="propsData.animation === 'zoomOut'"
+          :dropdown="propsData.animation === 'dropdown'"
+          :small="propsData.size === 'small'"
+          :large="propsData.size === 'large'"
+        >
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Explicabo ducimus dolorem excepturi facere architecto, quam commodi,
+            tenetur corrupti a eveniet laborum labore distinctio doloremque
+            eaque repellendus ab, culpa rem temporibus?
+          </p>
         </v-modal>
       </template>
+      <v-row>
+        <v-col c="6" class="u-center">
+          <v-dropdown>
+            <template v-slot:button>
+              <v-btn dropdown>
+                Animation <span class="icon"><i class="fa fa-wrapper fa-caret-down"></i></span>
+              </v-btn>
+            </template>
+            <v-dropdown-item
+              @click="(event) => { propsData.animation = 'zoomIn' }"
+            >ZoomIn</v-dropdown-item>
+            <v-dropdown-item
+              @click="(event) => { propsData.animation = 'zoomOut' }"
+            >ZoomOut</v-dropdown-item>
+            <v-dropdown-item
+              @click="(event) => { propsData.animation = 'dropdown' }"
+            >Dropdown</v-dropdown-item>
+          </v-dropdown>
+        </v-col>
+        <v-col c="6" class="u-center">
+          <v-dropdown>
+            <template v-slot:button>
+              <v-btn dropdown>
+                Size <span class="icon"><i class="fa fa-wrapper fa-caret-down"></i></span>
+              </v-btn>
+            </template>
+            <v-dropdown-item
+              @click="(event) => { propsData.size = 'small' }"
+            >Small</v-dropdown-item>
+            <v-dropdown-item
+              @click="(event) => { propsData.size = 'large' }"
+            >Large</v-dropdown-item>
+          </v-dropdown>
+        </v-col>
+      </v-row>
     </Playground>
 
     <v-space xlarge />
@@ -52,7 +98,9 @@ export default {
 
   data() {
     return {
-      propsData: {},
+      propsData: {
+        animation: '',
+      },
       props: [
         ['v-modal', 'title', 'string', 'Modal Dialog', 'Set the title of the modal.'],
         ['v-modal', 'closeTarget', 'string', '#target', 'When the modal closes this will be the target link.'],
