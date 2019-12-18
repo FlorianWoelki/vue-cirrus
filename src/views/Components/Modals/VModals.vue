e4406d571e2634a7e1745846dd6b61689d9d3ebd
FlorianWoelki <FlorianWoelki@gmx.de>
2019-12-18 10:18:38 +0100

    Add showcase for modals component

--- src/views/Components/Modals/VModals.vue
+++ src/views/Components/Modals/VModals.vue
@@ -1,11 +1,83 @@
 <template>
   <section class="modals-component">
-    <h1 class="u-text-center">Coming soon</h1>
-    <p class="font-thin u-text-center">This site will become perfect really soon.</p>
+    <p class="no-upper-margin">
+      Modals are there to create a small window in the browser
+      itself. You can easily modify anything you want!
+    </p>
+
+    <v-space xlarge />
+
+    <h3>Usage</h3>
+    <p class="no-upper-margin">
+      Modals in their simplest form have no animation or size.
+    </p>
+
+    <v-space />
+    <Playground
+      noTitle
+      href="/#/components/Modals/#!"
+      component="v-menu"
+      :componentProps="propsData"
+      :customCode="customCode"
+    >
+      <template v-slot:component>
+        <a href="#modal">
+          <v-btn class="u-center" color="success">Open Modal</v-btn>
+        </a>
+        <v-modal zoomIn large>
+          <p>testing</p>
+        </v-modal>
+      </template>
+    </Playground>
+
+    <v-space xlarge />
+    <h3 style="margin-bottom: 10px">API</h3>
+    <API :data="props" :dropdownItems="['v-modal']" />
+
+    <v-space xlarge />
+    <v-space xlarge />
+    <h3>Examples</h3>
   </section>
 </template>
 
 <script>
+import API from '@/views/Components/API.vue';
+import Playground from '@/components/Playground/Playground.vue';
+
 export default {
+  components: {
+    API,
+    Playground,
+  },
+
+  data() {
+    return {
+      propsData: {},
+      props: [
+        ['v-modal', 'title', 'string', 'Modal Dialog', 'Set the title of the modal.'],
+        ['v-modal', 'closeTarget', 'string', '#target', 'When the modal closes this will be the target link.'],
+        ['v-modal', 'mId', 'string', 'modal', 'Set the original modal id for further reference.'],
+        ['v-modal', 'small', 'boolean', 'false', 'Set the size of the modal to small.'],
+        ['v-modal', 'large', 'boolean', 'false', 'Set the size of the modal to large.'],
+        ['v-modal', 'zoomIn', 'boolean', 'false', 'Set the animation to zoom in.'],
+        ['v-modal', 'zoomOut', 'boolean', 'false', 'Set the animation to zoom out.'],
+        ['v-modal', 'dropdown', 'boolean', 'false', 'Set the animation to a dropdown.'],
+      ],
+    };
+  },
+
+
+  computed: {
+    customCode() {
+      return `
+<a href="#modal">
+  <v-btn class="u-center" color="success">Open Modal</v-btn>
+</a>
+<v-modal zoomIn large>
+  <p>testing</p>
+</v-modal>
+`;
+    },
+  },
 };
 </script>
