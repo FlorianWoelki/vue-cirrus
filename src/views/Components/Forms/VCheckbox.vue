8b25c7b903ff916bcb77c4172bc01323901e1f0c
florianwoelki <florianwoelki@gmx.de>
2019-12-22 12:04:01 +0100

    Add checkbox component showcase view

--- src/views/Components/Forms/VCheckbox.vue
+++ src/views/Components/Forms/VCheckbox.vue
@@ -1,11 +1,61 @@
 <template>
   <section class="checkbox-component">
-    <h1 class="u-text-center">Coming soon</h1>
-    <p class="font-thin u-text-center">This site will become perfect really soon.</p>
+    <p class="no-upper-margin">
+      Simply use the <kbd>v-checkbox</kbd> component to create easy checkboxes
+      for your single page application.
+    </p>
+
+    <v-space xlarge />
+
+    <h3>Usage</h3>
+    <p class="no-upper-margin">
+      The default checkbox is unselected and has a red tick.
+    </p>
+
+    <v-space />
+    <Playground
+      noTitle
+      href="/#/components/Checkbox/#!"
+      component="v-checkbox"
+      :componentProps="propsData"
+    >
+      <template v-slot:component>
+        <v-checkbox class="u-center">Customize Me</v-checkbox>
+      </template>
+    </Playground>
+
+    <v-space xlarge />
+    <h3 style="margin-bottom: 10px">API</h3>
+    <API :data="props" :dropdownItems="['v-checkbox']" />
+
+    <v-space xlarge />
+    <v-space xlarge />
+    <h3>Examples</h3>
   </section>
 </template>
 
 <script>
+import API from '../API.vue';
+import Playground from '@/components/Playground/Playground.vue';
+
 export default {
+  components: {
+    API,
+    Playground,
+  },
+
+  data() {
+    return {
+      propsData: {},
+      props: [
+        ['v-checkbox', 'id', 'string', 'input-checkbox', 'You need to set the id, if you want to have more checkboxes.'],
+        ['v-checkbox', 'checked', 'boolean', 'false', 'Set the checked state of the checkbox.'],
+        ['v-checkbox', 'success', 'boolean', 'false', 'Set the color of the checkbox to a green one.'],
+        ['v-checkbox', 'error', 'boolean', 'false', 'Set the color of the checkbox to a red one.'],
+        ['v-checkbox', 'disabled', 'boolean', 'false', 'Set the disabled state of the checkbox.'],
+        ['v-checkbox', '@change', 'function', '() => {}', 'Get the event, when a checkbox has changed.'],
+      ],
+    };
+  },
 };
 </script>
