e742f800ab02bf681530b6bc64b8c2e88ba4e2f8
florianwoelki <florianwoelki@gmx.de>
2019-12-29 14:36:59 +0100

    Add input color component showcase

--- src/views/Components/Forms/VInputColor.vue
+++ src/views/Components/Forms/VInputColor.vue
@@ -1,11 +1,55 @@
 <template>
-  <section class="input-color-component">
-    <h1 class="u-text-center">Coming soon</h1>
-    <p class="font-thin u-text-center">This site will become perfect really soon.</p>
+  <section class="date-picker-component">
+    <p class="no-upper-margin">
+      Use a custom input color form to let the user select the color he wants.
+    </p>
+
+    <v-space xlarge />
+
+    <h3>Usage</h3>
+    <p class="no-upper-margin">
+      The default input color form has the default color black (#000) as a input.
+    </p>
+
+    <v-space />
+    <Playground
+      noTitle
+      href="/#/components/InputColor/#!"
+      component="v-input-color"
+      :componentProps="propsData"
+    >
+      <template v-slot:component>
+        <v-input-color></v-input-color>
+      </template>
+    </Playground>
+
+    <v-space xlarge />
+    <h3 style="margin-bottom: 10px">API</h3>
+    <API :data="props" :dropdownItems="['v-input-color']" />
+
+    <v-space xlarge />
+    <v-space xlarge />
+    <h3>Examples</h3>
   </section>
 </template>
 
 <script>
+import API from '../API.vue';
+import Playground from '@/components/Playground/Playground.vue';
+
 export default {
+  components: {
+    API,
+    Playground,
+  },
+
+  data() {
+    return {
+      propsData: {},
+      props: [
+        ['v-input-color', 'defaultValue', 'string', '#000', 'Set the default color of the input color picker.'],
+      ],
+    };
+  },
 };
 </script>
