53d205cbc20880d3500e65c30e3ab9a77e764f2d
FlorianWoelki <FlorianWoelki@gmx.de>
2020-02-27 11:48:50 +0100

    fix(component): navbar disableMobileNavbar property and mobile navbar version

--- src/components/Navbar/VNavbar.vue
+++ src/components/Navbar/VNavbar.vue
@@ -6,7 +6,9 @@
       'header-dark': this.dark,
       'header-clear': this.clear,
     },
-  ]">
+  ]"
+    :style="`${disableMobileNavbar ? 'flex-direction: initial;' : null}`"
+  >
     <div class="header-brand">
       <div
         class="nav-item no-hover"
@@ -62,6 +64,7 @@ export default {
   mounted() {
     if (!this.disableMobileNavbar) {
       const headerBtn = document.getElementById('header-btn');
+      headerBtn.style.alignSelf = 'center';
 
       headerBtn.addEventListener('click', () => {
         if (document.getElementById('header-menu').classList.contains('active')) {
@@ -74,6 +77,9 @@ export default {
           // document.querySelector('#header').classList.add('translucent');
         }
       });
+    } else {
+      document.getElementById('header-menu').style.height = 'initial';
+      document.getElementById('header-menu').style.alignSelf = 'center';
     }
   },
 };
