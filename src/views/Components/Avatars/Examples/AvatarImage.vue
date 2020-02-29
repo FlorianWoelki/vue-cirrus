75dd52e5c9e17fe800e29a6ba15d670945c02937
florianwoelki <florianwoelki@gmx.de>
2020-02-29 10:50:09 +0100

    fix(component): avatar examples

--- src/views/Components/Avatars/Examples/AvatarImage.vue
+++ src/views/Components/Avatars/Examples/AvatarImage.vue
@@ -1,16 +1,16 @@
 <template>
   <v-row class="avatar-example-size u-text-center">
     <v-col c="3">
-      <v-avatar xsmall src="https://randomuser.me/api/portraits/men/30.jpg" />
+      <v-avatar size="xsmall" src="https://randomuser.me/api/portraits/men/30.jpg" />
     </v-col>
     <v-col c="3">
-      <v-avatar small src="https://randomuser.me/api/portraits/men/30.jpg" />
+      <v-avatar size="small" src="https://randomuser.me/api/portraits/men/30.jpg" />
     </v-col>
     <v-col c="3">
-      <v-avatar large src="https://randomuser.me/api/portraits/men/30.jpg" />
+      <v-avatar size="large" src="https://randomuser.me/api/portraits/men/30.jpg" />
     </v-col>
     <v-col c="3">
-      <v-avatar xlarge src="https://randomuser.me/api/portraits/men/30.jpg" />
+      <v-avatar size="xlarge" src="https://randomuser.me/api/portraits/men/30.jpg" />
     </v-col>
   </v-row>
 </template>
--- src/views/Components/Avatars/Examples/AvatarSize.vue
+++ src/views/Components/Avatars/Examples/AvatarSize.vue
@@ -1,19 +1,19 @@
 <template>
   <v-row level class="avatar-example-size u-text-center">
     <v-col c="3">
-      <v-avatar xsmall text="FW" />
+      <v-avatar size="xsmall" text="FW" />
       <span class="font-thin">xsmall</span>
     </v-col>
     <v-col c="3">
-      <v-avatar small text="FW" />
+      <v-avatar size="small" text="FW" />
       <span class="font-thin">small</span>
     </v-col>
     <v-col c="3">
-      <v-avatar large text="FW" />
+      <v-avatar size="large" text="FW" />
       <span class="font-thin">large</span>
     </v-col>
     <v-col c="3">
-      <v-avatar xlarge text="FW" />
+      <v-avatar size="xlarge" text="FW" />
       <span class="font-thin">xlarge</span>
     </v-col>
   </v-row>
