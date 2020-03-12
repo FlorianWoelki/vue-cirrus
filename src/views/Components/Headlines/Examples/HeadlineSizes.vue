3a6cccbbc4feee8af1ab7317de1cf313086fc1b0
FlorianWoelki <FlorianWoelki@gmx.de>
2020-03-12 12:47:14 +0100

    style: set alignment to headline sizes example to center

--- src/views/Components/Headlines/Examples/HeadlineSizes.vue
+++ src/views/Components/Headlines/Examples/HeadlineSizes.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row class="headline-example-section">
-    <v-col c="12">
+    <v-col c="12 u-text-center">
       <v-headline size="1">1 Size</v-headline>
       <v-headline size="2">2 Size</v-headline>
       <v-headline size="3">3 Size</v-headline>
