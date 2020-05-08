bc7cf35f0374aa354708b580c96cb66bd7266708
florianwoelki <florianwoelki@gmx.de>
2020-05-08 09:15:51 +0200

    refactor(v-link): animations to one animation prop

--- src/components/Link/VLink.vue
+++ src/components/Link/VLink.vue
@@ -1,10 +1,26 @@
 <template>
   <span
-    :class="squared ? 'usquare' : dSquared ? 'usquare delay' : null"
+    :class="animation === 'squared' ? 'usquare' : animation === 'dSquared' ? 'usquare delay' : null"
   >
     <a
       :href="href"
-      :class="classes"
+      :class="[
+        this.tooltipMixins,
+        this.animationsMixins,
+        {
+          'u u-LR': this.animation === 'ltr',
+          'u u-RL': this.animation === 'rtl',
+          'u u-C': this.animation === 'c',
+          'utb utb-LR': this.animation === 'oltr',
+          'utb utb-RL': this.animation === 'ortl',
+          'utb utb-C': this.animation === 'oc',
+          'utb utb-OLR': this.animation === 'olr',
+          'utb utb-ORL': this.animation === 'orl',
+          'utb utb-OLR sq': this.animation === 'squared',
+          'utb utb-OLR delay': this.animation === 'dSquared',
+          underline: this.animation === 'underlined',
+        },
+      ]"
       :target="blank ? '_blank' : ''"
       :data-tooltip=tooltipData
       @click="$emit('click', $event)"
@@ -33,71 +49,13 @@ export default {
       type: Boolean,
       default: false,
     },
-    ltr: {
-      type: Boolean,
-      default: false,
-    },
-    rtl: {
-      type: Boolean,
-      default: false,
-    },
-    c: {
-      type: Boolean,
-      default: false,
-    },
-    oltr: {
-      type: Boolean,
-      default: false,
-    },
-    ortl: {
-      type: Boolean,
-      default: false,
-    },
-    oc: {
-      type: Boolean,
-      default: false,
-    },
-    olr: {
-      type: Boolean,
-      default: false,
-    },
-    orl: {
-      type: Boolean,
-      default: false,
-    },
-    squared: {
-      type: Boolean,
-      default: false,
-    },
-    dSquared: {
-      type: Boolean,
-      default: false,
-    },
-    underlined: {
-      type: Boolean,
-      default: false,
-    },
-  },
-
-  computed: {
-    classes() {
-      return Object.assign(
-        this.tooltipMixins,
-        this.animationsMixins,
-        {
-          'u u-LR': this.ltr,
-          'u u-RL': this.rtl,
-          'u u-C': this.c,
-          'utb utb-LR': this.oltr,
-          'utb utb-RL': this.ortl,
-          'utb utb-C': this.oc,
-          'utb utb-OLR': this.olr,
-          'utb utb-ORL': this.orl,
-          'utb utb-OLR sq': this.squared,
-          'utb utb-OLR delay': this.dSquared,
-          underline: this.underlined,
-        },
-      );
+    animation: {
+      type: String,
+      default: null,
+      validator: value => [
+        'ltr', 'rtl', 'c', 'oltr', 'ortl', 'oc', 'olr',
+        'orl', 'underlined', 'squared', 'dSquared',
+      ].indexOf(value) !== -1,
     },
   },
 };
