9a4a6768b9509eb6fd3edd1fe3a19155d79d89ff
florianwoelki <florianwoelki@gmx.de>
2020-11-07 09:20:12 +0100

    fix(tile): remove and change some styles

--- src/components/Tile/VTile.vue
+++ src/components/Tile/VTile.vue
@@ -2,13 +2,12 @@
   <div
     :class="[
       'tile',
-      'tile--center',
       {
         'dark': this.dark,
       },
     ]"
     :style="enableBoxShadow ? 'box-shadow: 0 3px 6px rgba(0,0,0,0.06)' +
-      ', 0 3px 6px rgba(0,0,0,0.03); padding: 0.25rem 1rem' : null"
+      ', 0 3px 6px rgba(0,0,0,0.03); padding: 1rem 2rem' : null"
   >
     <div class="tile__icon">
       <slot name="icon"></slot>
