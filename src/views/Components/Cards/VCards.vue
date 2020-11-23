aaf8d53abdf60bb59e4d7ae399d184bf3737708e
florianwoelki <florianwoelki@gmx.de>
2020-11-23 08:19:24 +0100

    chore(card): fix card code playground

--- src/views/Components/Cards/VCards.vue
+++ src/views/Components/Cards/VCards.vue
@@ -21,17 +21,17 @@
       :customCode="customCode"
     >
       <template v-slot:component>
-        <v-card>
-          <v-card-title>{{ propsData.title }}</v-card-title>
-          <v-card-content>
-            <v-space></v-space>
-            <v-avatar text="FW"></v-avatar>
+        <v-card style="max-width: 250px">
+          <v-container class="u-text-center pt-3">
+            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bolt" className="svg-inline--fa fa-bolt fa-w-10 fa-wrapper text-blue-600 bg-blue-100 p-3" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" style="border-radius: 100%; height: 4.75rem; width: 4.75rem;">
+              <!-- eslint-disable-next-line -->
+              <path fill="currentColor" d="M296 160H180.6l42.6-129.8C227.2 15 215.7 0 200 0H56C44 0 33.8 8.9 32.2 20.8l-32 240C-1.7 275.2 9.5 288 24 288h118.7L96.6 482.5c-3.6 15.2 8 29.5 23.3 29.5 8.4 0 16.4-4.4 20.8-12l176-304c9.3-15.9-2.2-36-20.7-36z"></path>
+            </svg>
+            <v-card-title>
+              <p>{{ propsData.title }}</p>
+            </v-card-title>
             <p>{{ propsData.content }}</p>
-            <a href="#">#vue-cirrus</a>
-            <a href="#">#cirrus</a>
-          </v-card-content>
-
-          <v-card-footer>{{ propsData.footer }}</v-card-footer>
+          </v-container>
         </v-card>
       </template>
       <v-row>
@@ -95,18 +95,16 @@ export default {
 
   computed: {
     customCode() {
-      return `
-<v-card>
-  <v-card-title>${this.propsData.title}</v-card-title>
-  <v-card-content>
-    <v-space></v-space>
-    <v-avatar text="FW"></v-avatar>
+      return `<v-card style="max-width: 250px">
+  <v-container class="u-text-center pt-3">
+    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bolt" className="svg-inline--fa fa-bolt fa-w-10 fa-wrapper text-blue-600 bg-blue-100 p-3" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" style="border-radius: 100%; height: 4.75rem; width: 4.75rem;">
+      <path fill="currentColor" d="M296 160H180.6l42.6-129.8C227.2 15 215.7 0 200 0H56C44 0 33.8 8.9 32.2 20.8l-32 240C-1.7 275.2 9.5 288 24 288h118.7L96.6 482.5c-3.6 15.2 8 29.5 23.3 29.5 8.4 0 16.4-4.4 20.8-12l176-304c9.3-15.9-2.2-36-20.7-36z"></path>
+    </svg>
+    <v-card-title>
+      <p>${this.propsData.title}</p>
+    </v-card-title>
     <p>${this.propsData.content}</p>
-    <a href="#">#vue-cirrus</a>
-    <a href="#">#cirrus</a>
-  </v-card-content>
-
-  <v-card-footer>${this.propsData.footer}</v-card-footer>
+  </v-container>
 </v-card>
 `;
     },
