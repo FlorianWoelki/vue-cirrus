a4c355f5c650fb09ee02c157716581f968a9d9fb
florianwoelki <florianwoelki@gmx.de>
2020-11-23 08:23:16 +0100

    fix(dropdown): example with arrow icon

--- src/views/Components/Dropdowns/Examples/DropdownArrows.vue
+++ src/views/Components/Dropdowns/Examples/DropdownArrows.vue
@@ -3,8 +3,9 @@
     <v-col c="12" class="u-center">
       <v-dropdown>
         <template v-slot:button>
-          <v-btn dropdown>
-            FA Arrow <span class="icon"><i class="fa fa-wrapper fa-caret-down"></i></span>
+          <v-btn dropdown class="u-flex u-items-center">
+            <span>Dropdown</span>
+            <svg style="width: 1.5em; height: 1.5em" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
           </v-btn>
         </template>
         <v-dropdown-item>Option A</v-dropdown-item>
