9c51c4b2241b3e6cd9c2e89b04dc8cdc9a232690
florianwoelki <florianwoelki@gmx.de>
2020-11-26 08:58:17 +0100

    feat(comp): add Grid component

---
+++ src/components/Grid/VGrid.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="[
+    'grid',
+    gap ? `grid-gap-${gap}` : null,
+    cols ? `grid-cols-${cols}` : null
+  ]">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    gap: String,
+    cols: String,
+  },
+};
+</script>
