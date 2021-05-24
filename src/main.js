f05db6270bb4b058f4b20df10a1453dd67ca118a
FlorianWoelki <FlorianWoelki@gmx.de>
2021-05-24 12:18:17 +0200

    build: fix linting issues with editorconfig

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-const install = (Vue) => {
-  const requireComponent = require.context('./components', true, /V[\w-]+.vue$/);
+const install = (app) => {
+  const requireComponent = require.context(
+    './components',
+    true,
+    /V[\w-]+.vue$/,
+  );
 
   requireComponent.keys().forEach((filename) => {
     const componentConfig = requireComponent(filename);
@@ -11,13 +15,9 @@ const install = (Vue) => {
       .toLowerCase()
       .replace('v', 'v-')}`;
 
-    Vue.component(componentName, componentConfig.default || componentConfig);
+    app.component(componentName, componentConfig.default || componentConfig);
   });
 };
-
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue);
-}
 
 export default install;
 
