a57dbfd2b6ba60c9c26e1ee1ae6ec800981f4772
FlorianWoelki <FlorianWoelki@gmx.de>
2021-05-24 11:14:41 +0200

    build: remove error with line length

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,12 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/airbnb'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'template-curly-spacing': 'off',
+    'max-len': ['off', { code: 80, ignoreUrls: true }],
     indent: 'off',
   },
   parserOptions: {
